106e9ca4567bcc27ca4dc67fa221d58851236f24
samet7ozturk <sametozturk973@gmail.com>
2023-11-21 23:44:31 +0300

    Create favoritesReducer

---
+++ src/reducers/favoritesReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  favorites: [],
+  displayFavorites: false,
+};
+
+const favoritesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default favoritesReducer;
